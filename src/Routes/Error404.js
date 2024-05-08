add436e966ae24217ea09f1a8794134878090b0f
Emily Painter <rosepaintingemily@gmail.com>
2024-05-08 15:47:34 -0700

    update all links to have #

--- src/Routes/Error404.js
+++ src/Routes/Error404.js
@@ -8,7 +8,7 @@ export default function Login() {
             <div className="empty-state">
                 <h1 className="font-page-heading">Page Not Found</h1>
                 <p className="font-body">The page you are looking for does not exist.</p>
-                <Button variant="text" disableElevation href="/">
+                <Button variant="text" disableElevation href="#/">
                     <span className="font-body-bold">Return Home</span>
                 </Button>
             </div>
