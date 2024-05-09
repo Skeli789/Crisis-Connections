c7e102377cfb04624558798492443a37ad17d4a4
Emily Painter <rosepaintingemily@gmail.com>
2024-05-09 16:56:21 -0700

    add schema

--- src/utils/api.js
+++ src/utils/api.js
@@ -69,3 +69,284 @@ export const userFields = [
         value: ''
     }
 ]
+
+export const callerSchema = {
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "type": "object",
+  "properties": {
+    "id": {
+      "type": "string"
+    },
+    "aka": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      }
+    },
+    "zip": {
+      "type": "string"
+    },
+    "city": {
+      "type": "string"
+    },
+    "county": {
+      "type": "string"
+    },
+    "gender": {
+      "type": "array",
+      "items": {
+        "type": "string",
+        "enum": ["male","female","non-binary","transgender","other","unknown"]
+      }
+    },
+    "archived": {
+      "type": "object",
+      "properties": {
+        "isArchived": {
+          "type": "boolean"
+        },
+        "by": {
+          "type": "string"
+        },
+        "dateTime": {
+          "type": "string"
+        },
+        "reason": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "isArchived"
+      ]
+    },
+    "birthday": {
+      "type": "string"
+    },
+    "lastName": {
+      "type": "string"
+    },
+    "ethnicity": {
+      "type": "array",
+      "items": [
+        {
+          "type": "string",
+          "enum": [
+            "americanIndian",
+              "asianIndian",
+              "africanAmerican",
+              "cambodian",
+              "chinese",
+              "filipino",
+              "guamanian",
+              "japanese",
+              "korean",
+              "laotian",
+              "middleEastern",
+              "nativeHawaiian",
+              "otherPacificIslander",
+              "white",
+              "unknown",
+              "otherRace",
+              "cuban",
+              "hispanicSpecificOriginUnknown",
+              "mexican",
+              "hispanic",
+              "puertoRican"
+          ]
+        }
+      ]
+    },
+    "firstName": {
+      "type": "string"
+    },
+    "insurance": {
+      "type": "array",
+      "items": [
+        {
+          "type": "string",
+          "enum": [
+            "unknown","medicaid","medicare","other","private","va","none"
+          ]
+        }
+      ]
+    },
+    "callHistory": {
+      "type": "array",
+      "items": [
+        {
+          "type": "object",
+          "properties": {
+            "with": {
+              "type": "string"
+            },
+            "notes": {
+              "type": "string"
+            },
+            "service": {
+              "type": "string",
+              "enum": ["988KingCounty",
+              "washingtonTeenLink",
+              "washingtonRecoveryHelpLine",
+              "washingtonWarmLine",
+              "crisisServices",
+              "211KingCounty",
+              "washingtonSupportAfterSuicide"]
+            },
+            "dateTime": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "with",
+            "notes",
+            "service",
+            "dateTime"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "with": {
+              "type": "string"
+            },
+            "notes": {
+              "type": "string"
+            },
+            "service": {
+              "type": "string"
+            },
+            "dateTime": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "with",
+            "notes",
+            "service",
+            "dateTime"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "with": {
+              "type": "string"
+            },
+            "notes": {
+              "type": "string"
+            },
+            "service": {
+              "type": "string"
+            },
+            "dateTime": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "with",
+            "notes",
+            "service",
+            "dateTime"
+          ]
+        },
+        {
+          "type": "object",
+          "properties": {
+            "with": {
+              "type": "string"
+            },
+            "notes": {
+              "type": "string"
+            },
+            "service": {
+              "type": "string"
+            },
+            "dateTime": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "with",
+            "notes",
+            "service",
+            "dateTime"
+          ]
+        }
+      ]
+    },
+    "lastUpdated": {
+      "type": "object",
+      "properties": {
+        "by": {
+          "type": "string"
+        },
+        "dateTime": {
+          "type": "string"
+        }
+      },
+      "required": [
+        "by",
+        "dateTime"
+      ]
+    },
+    "phoneNumbers": {
+      "type": "array",
+      "items": [
+        {
+          "type": "integer"
+        }
+      ]
+    },
+    "relevantInfo": {
+      "type": "string"
+    },
+    "sexualOrientation": {
+      "type": "array",
+      "items": [
+        {
+          "type": "string",
+          "enum": ["heterosexual","lgbtq","bisexual","unknown","undisclosed"]
+        }
+      ]
+    },
+    "specificInstructions": {
+      "type": "string"
+    },
+    "currentBehavioralTreatment": {
+      "type": "array",
+      "items": [
+        {
+          "type": "object",
+          "properties": {
+            "notes": {
+              "type": "string"
+            },
+            "location": {
+              "type": "string"
+            },
+            "undergoing": {
+              "type": "string",
+              "enum": [
+                "yes","no","unknown"
+              ]
+            }
+          },
+          "required": [
+            "notes",
+            "location",
+            "undergoing"
+          ]
+        }
+      ]
+    }
+  },
+  "required": [
+    "id",
+    "archived",
+    "callHistory",
+    "lastUpdated",
+    "phoneNumbers",
+    "relevantInfo",
+    "specificInstructions"
+  ]
+};
